0d1668bce81dc29e205100e01d8927cac0561e93
Moshe Piekarski <dev.github@melachim.net>
2020-11-30 23:06:01 -0500

    Add template

---
+++ template.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+int main (int argc, char * argv[])
+{
+    char *filename = "input";
+    int result=0;
+    int part=1;
+    char buf[16];
+    FILE * fp;
+    if(argc > 1)
+        part=atoi(argv[1]);
+    if(argc > 2)
+        filename=argv[2];
+    if((fp = fopen (filename, "r"))==NULL)
+    {
+        printf("error opening file %s\n", filename);
+        return 1;
+    }
+    while(fgets(buf, 16, fp) != NULL)
+    {
+    }
+    printf("%d\n",result);
+    return 0;
+}
