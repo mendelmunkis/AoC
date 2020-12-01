c5154a2534787bfe94302dc88d1fddddf18cf863
Moshe Piekarski <dev.github@melachim.net>
2020-12-01 05:08:44 -0500

    Fix download script bug

--- download.sh
+++ download.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 YEAR=`date +%Y`
-DAY=`date +%d`
+DAY=`date +%-d`
 wget "https://adventofcode.com/$YEAR/day/$DAY/input" --header "Cookie: session=$(cat session_cookie)"  -O $YEAR/inputs/input$DAY
 cp -f $YEAR/inputs/input$DAY $YEAR/input
