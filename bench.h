cd166e2075adada8c3f9e6eaaa58b6134e4d4f4b
Moshe Piekarski <dev.github@melachim.net>
2020-12-03 15:33:17 -0500

    benhmarking header

---
+++ bench.h
@@ -0,0 +1,5 @@
+#include <time.h>
+
+#define START float starttime = (float)clock()/(CLOCKS_PER_SEC/1000);
+#define END float endtime = (float)clock()/(CLOCKS_PER_SEC/1000);
+#define TIME endtime-starttime
