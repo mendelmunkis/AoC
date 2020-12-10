cee35ca9488eb1193f5096470b472e9af855ae58
Moshe Piekarski <dev.github@melachim.net>
2020-12-10 15:12:10 -0500

    add further compiler optimizations

--- 2020/assembler.c
+++ 2020/assembler.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-int digits(int in);
+#define MAX_LEN 1000
 
 int main (int argc, char * argv[])
 {
@@ -18,61 +18,75 @@ int main (int argc, char * argv[])
         return 1;
     }
 
-    short location[1000][2]={0};
+    int location[MAX_LEN][3]={0};
     char buf[16];
     int counter=0,pc=0;
-    int target,total,jump;
+    int target,total,jump,acc;
     fputs("1106,0,4,0,",outfp);
     pc+=4;
     while(fgets(buf, 16, infp) != NULL)
     {
         if(strncmp(buf, "acc",3)==0)
         {
-            fprintf(outfp,"101,%d,3,3,",strtol(buf+3,NULL,10));
-            location[counter][0]=pc;
-            pc+=4;
+            location[counter][2]=strtol(buf+3,NULL,10);
+            location[counter][0]=1;
         }
         if(strncmp(buf, "jmp",3)==0)
         {
-            fprintf(outfp,"1106,0,0000,");
-            location[counter][1]=strtol(buf+3,NULL,10)+counter;
-            location[counter][0]=pc;
-            pc+=3;
+            location[counter][2]=strtol(buf+3,NULL,10)+counter;
+            location[counter][0]=2;
+            location[location[counter][2]][1]=1;
         }
         counter++;
     }
-    fputs("4,3,99",outfp);
-    total=pc+3;
-    location[counter][0]=pc;
-    pc=0;
-    rewind(outfp);
     for(int i=0;i<counter;i++)
     {
-        jump=location[i][1];
-        if(jump)
+        if(location[i][0]==1)
         {
+            if(!location[i][1])
+            {
+                for(acc=i-1;!(location[acc][0]||location[acc][1]);acc--);
+                if(location[acc][0]==1)
+                {
+                    location[acc][2]+=location[i][2];
+                    location[i][0]=0;
+                }
+                else
+                {
+                    location[i][1]=pc;
+                    pc+=4;
+                }
+            }
+            else
+            {
+                location[i][1]=pc;
+                pc+=4;
+            }
+        }
+        else if(location[i][0]==2){
+            location[i][1]=pc;pc+=3;}//removethis
+    }
+    total=pc+3;
+    location[counter][0]=2;
+    location[counter][1]=pc;
+    for(int i=0;i<counter;i++)
+    {
+        if(location[i][0]==1)
+            fprintf(outfp,"101,%d,3,3,",location[i][2]);
+        else if(location[i][0]==2)
+        {
+             jump=location[i][2];
              if((jump<0)||(jump>counter))
                  target=total;
              else
              {
                  while(!location[jump][0])
                      jump++;
-                 target=location[jump][0];
+                 target=location[jump][1];
              }
-             for(;pc<location[i][0]+2;pc++)
-                 while(fgetc(outfp)!=','){}
-             for(int j=0;j<(4-digits(target));j++)
-                 fgetc(outfp);
-             fprintf(outfp,"%d",target);
+             fprintf(outfp,"1106,0,%d,",target);
         }
     }
+    fputs("4,3,99",outfp);
     return 0;
-}
-
-int digits(int in)
-{
-    int counter=1;
-    while(in/=10)
-        counter++;
-    return counter;
 }
